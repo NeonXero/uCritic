56661cf41ffa9569980fcf53cd20c2f9b09555fb
Scott A Garman <sag5066@gmail.com>
2012-09-28 16:21:45 -0700

    Adding better support for url generation. Fixing EditText query creation.

--- src/net/neonlotus/ucritic/MyActivity.java
+++ src/net/neonlotus/ucritic/MyActivity.java
@@ -12,32 +12,31 @@ import com.google.gson.Gson;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIUtils;
+import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.List;
 // http://www.javacodegeeks.com/2011/01/android-json-parsing-gson-tutorial.html
 // http://developer.rottentomatoes.com/docs/read/json/v10/Movie_Info
 // http://jsonviewer.stack.hu/#http://api.rottentomatoes.com/api/public/v1.0/movies/770672122.json?apikey=vg2cj5tgqmbkkxz2vgyxqyh9
 
 public class MyActivity extends Activity {
+    // Constants
+    private static final String ROTTEN_TOMATOES_API_KEY = "vg2cj5tgqmbkkxz2vgyxqyh9";
 
-	TextView t1;
-	EditText e1;
-	Button b1;
-	// url to make request
-	static String myAPI = "vg2cj5tgqmbkkxz2vgyxqyh9";
-	//static String movieID = "770672122";
-	static String movieID = "";
-	static String movieTitle = "";
-
-
-	private static String movieQuery = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey="+myAPI+"&q="+movieTitle+"&page_limit=10";
-	private static String url = "http://api.rottentomatoes.com/api/public/v1.0/movies/"+movieID+".json?apikey="+myAPI;
+    // Views
+	private TextView t1;
+	private EditText e1;
+	private Button b1;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -49,43 +48,40 @@ public class MyActivity extends Activity {
 		b1.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				movieTitle = e1.getText().toString().replace(" ","+");
-				String temp2 = "";
+                String movieTitleFromEditText = e1.getText().toString().replace(" ","+");
+                String movieQueryUrl = generateMovieQueryUrl(movieTitleFromEditText);
 
 				//Toast.makeText(MyActivity.this, movieTitle,Toast.LENGTH_SHORT).show();
-
-				InputStream source = retrieveStream(movieQuery);
+                Log.d("ucritic", "request url: " + movieQueryUrl);
+				InputStream source = retrieveStream(movieQueryUrl);
 				Gson gson = new Gson();
 				Reader reader = new InputStreamReader(source);
 				Query mQuery = gson.fromJson(reader, Query.class);
-				List<Movie> mMovie = mQuery.movies;
+
+                List<Movie> mMovie = mQuery.movies;
 
 				for (Movie movie : mMovie) { //Could not find dictionary pack???
-					Toast.makeText(MyActivity.this, movie.id,Toast.LENGTH_SHORT).show();
+					Toast.makeText(MyActivity.this, movie.id, Toast.LENGTH_SHORT).show();
 				}
-
-				t1.setText(temp2);
 			}
 		});
-
-		InputStream source = retrieveStream(url);
-
-		Gson gson = new Gson();
-
-		Reader reader = new InputStreamReader(source);
-
-		MovieObject mObject = gson.fromJson(reader, MovieObject.class); //Expected BEGIN_ARRAY but was BEGIN_OBJECT instead. Something to do with the Rating object I believe.
-		Rating ratings = mObject.ratings;
-
-		//Toast.makeText(this, mObject.title, Toast.LENGTH_SHORT).show();
-		//Toast.makeText(this, mObject.synopsis,Toast.LENGTH_SHORT).show();
-
-		//Toast.makeText(this, String.valueOf(ratings.criticsScore),Toast.LENGTH_SHORT).show();
-
-
-
-
 	}
+
+    private void getMovieDataWithId(String movieID) {
+        InputStream source = retrieveStream(generateMovieUrl(movieID));
+
+        Gson gson = new Gson();
+
+        Reader reader = new InputStreamReader(source);
+
+        MovieObject mObject = gson.fromJson(reader, MovieObject.class); //Expected BEGIN_ARRAY but was BEGIN_OBJECT instead. Something to do with the Rating object I believe.
+        Rating ratings = mObject.ratings;
+
+        //Toast.makeText(this, mObject.title, Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, mObject.synopsis,Toast.LENGTH_SHORT).show();
+
+        //Toast.makeText(this, String.valueOf(ratings.criticsScore),Toast.LENGTH_SHORT).show();
+    }
 
 	private InputStream retrieveStream(String url) {
 
@@ -115,8 +111,76 @@ public class MyActivity extends Activity {
 		return null;
 	}
 
+    /**
+     * Generates a movie search(query) url based on input movie title (query)
+     *
+     * @param movieTitle - movie title
+     * @return
+     */
+    private String generateMovieQueryUrl(String movieTitle) {
+        URI uri = null;
+        List<NameValuePair> qparams = new ArrayList<NameValuePair>();
+        qparams.add(new BasicNameValuePair("apikey", ROTTEN_TOMATOES_API_KEY));
+        qparams.add(new BasicNameValuePair("q", movieTitle));
+        qparams.add(new BasicNameValuePair("page_limit", "10"));
+        try {
+            uri = URIUtils.createURI("http", "api.rottentomatoes.com", -1, "/api/public/v1.0/movies.json", URLEncodedUtils.format(qparams, "UTF-8"), null);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        return (uri != null) ? uri.toString() : null;
+    }
 
 
+    /**
+     * Generates a movie search(query) url based on input movie id
+     *
+     * @param movieID - movie title
+     * @return
+     *
+     * //TODO: make these two functions one function
+     */
+    private String generateMovieUrl(String movieID) {
+        //private String url = "http://api.rottentomatoes.com/api/public/v1.0/movies/"+movieID+".json?apikey="+ROTTEN_TOMATOES_API_KEY;
+
+        StringBuilder movieDataPathBuilder = new StringBuilder();
+        movieDataPathBuilder.append("/api/public/v1.0/movies/");
+        try {
+            movieDataPathBuilder.append(URLEncoder.encode(movieID, "UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        movieDataPathBuilder.append(".json");
+
+        URI uri = null;
+        List<NameValuePair> qparams = new ArrayList<NameValuePair>();
+        qparams.add(new BasicNameValuePair("apikey", ROTTEN_TOMATOES_API_KEY));
+
+        try {
+            uri = URIUtils.createURI("http", "api.rottentomatoes.com", -1, movieDataPathBuilder.toString(), URLEncodedUtils.format(qparams, "UTF-8"), null);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        return (uri != null) ? uri.toString() : null;
+    }
 
 
+    /**
+     * Pseudo code example of combining these two functions
+     *
+     */
+    private String generateUrls(String path, List<NameValuePair> params){
+        URI uri = null;
+        try {
+            uri = URIUtils.createURI("http", "api.rottentomatoes.com", -1, path, URLEncodedUtils.format(params, "UTF-8"), null);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        return (uri != null) ? uri.toString() : null;
+    }
 }
